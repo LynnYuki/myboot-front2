508dfe15b122df205d809565c364ba972017d42f
lynnyuki <lynnyuki9527@163.com>
2021-01-23 17:25:40 +0800

    add axios

---
+++ src/api/index.js
@@ -0,0 +1,18 @@
+//将所有接口挂载到vue原型链上
+
+import apis from './interface'
+
+const install = Vue => {
+    if(install.installed) {return}
+
+    install.installed = true
+
+    Object.defineProperties(Vue.prototype,{
+        $api:{
+            get(){
+                return apis
+            }
+        }
+    })
+}
+export default install
---
+++ src/api/interfaceurls.js
@@ -0,0 +1,6 @@
+const apiUrls = {
+    test1:'/sayHellow',
+    test2:'/sayHellow2'
+
+}
+export default api
